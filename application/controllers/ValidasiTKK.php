066e84556816b3f7083a4ab92b135df58633e341
robyobby <robidinnoo1992@gmail.com>
2023-12-30 12:45:51 +0800

    update

--- application/controllers/ValidasiTKK.php
+++ application/controllers/ValidasiTKK.php
@@ -1,6 +1,9 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
 class ValidasiTKK extends CI_Controller
 {
    function __construct()
@@ -38,6 +41,32 @@ class ValidasiTKK extends CI_Controller
 
    public function excelMahasiswa()
    {
-      
+      // Load PhpSpreadsheet library
+      require_once APPPATH . 'third_party/PhpSpreadsheet/vendor/autoload.php';
+
+      // Create a new Spreadsheet object
+      $spreadsheet = new Spreadsheet();
+      $sheet = $spreadsheet->getActiveSheet();
+
+      // Add your data to the spreadsheet
+      $data = $this->your_model->getData(); // Adjust this line based on your model
+      $row = 1;
+
+      foreach ($data as $item) {
+         $sheet->setCellValue('A' . $row, $item->id);
+         $sheet->setCellValue('B' . $row, $item->nama);
+         // Add other columns as needed
+         $row++;
+      }
+
+      // Set headers for download
+      header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+      header('Content-Disposition: attachment;filename="export.xlsx"');
+      header('Cache-Control: max-age=0');
+
+      // Create Excel file
+      $writer = new Xlsx($spreadsheet);
+      $writer->save('php://output');
    }
+   
 }
